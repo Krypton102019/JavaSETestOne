e3add4b788265c938ba98c6bc305b1648411dd00
Krypton102019 <88681250+Krypton102019@users.noreply.github.com>
2023-02-18 15:19:13 +0630

    Update Q17.java

--- src/Q17.java
+++ src/Q17.java
@@ -1,10 +1,11 @@
 public class Q17 {
     public static void main(String[] args) {
-        for(int i=1;i<=2;i++){
-            for(int j=1;j<=2;j++){
-                System.out.println("*"+"*"+"*"+"*"+"*");
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print("*");
             }
+            System.out.println();
         }
-
+       
     }
 }
