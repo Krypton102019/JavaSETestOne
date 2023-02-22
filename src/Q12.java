17eebe68d45c4a6e9f9c961a11b4ac0ebb38e9b5
Krypton102019 <88681250+Krypton102019@users.noreply.github.com>
2023-02-22 21:27:47 +0630

    Update Q12.java

--- src/Q12.java
+++ src/Q12.java
@@ -2,24 +2,17 @@ import java.util.Arrays;
 
 public class Q12 {
     public static void main(String[] args) {
-      int[] arr = {1, 2, 3, 4, 5};
+       int[] arr2 = {1, 2, 3, 4, 5};
 
-        int index = 2; // the index of the element to be deleted
+        int index2 = 2; // the index of the element to be deleted
 
-// shift the elements after the deleted element to the left using a nested for loop
-        for (int i = index; i < arr.length - 1; i++) {
-            for (int j = i; j < arr.length - 1; j++) {
-                arr[j] = arr[j + 1];
-            }
-        }
-        System.out.println(Arrays.toString(arr));
+// use System.arraycopy() to delete the element
+        System.arraycopy(arr2, index2 + 1, arr2, index2, arr2.length - index2 - 1);
+        System.out.println(Arrays.toString(arr2));
 // decrease the size of the array by one
-        int[] newArr = new int[arr.length - 1];
-        for (int i = 0; i < newArr.length; i++) {
-            newArr[i] = arr[i];
-        }
-
-        System.out.println(Arrays.toString(newArr));
+        int[] newArr2 = new int[arr.length - 1];
+        System.arraycopy(arr2, 0, newArr2, 0, newArr2.length);
+        System.out.println(Arrays.toString(newArr2));
 
     }
 }
