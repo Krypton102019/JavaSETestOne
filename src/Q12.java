009fefa2494d45567f7b853da9e73649ae212fd7
Krypton102019 <88681250+Krypton102019@users.noreply.github.com>
2023-02-22 20:44:08 +0630

    Update Q12.java

--- src/Q12.java
+++ src/Q12.java
@@ -2,19 +2,24 @@ import java.util.Arrays;
 
 public class Q12 {
     public static void main(String[] args) {
-        int[] arr = new int[]{121,334,2435,534,155};
-        int[] arr2 = new int[arr.length-1];
-        int deletedPosition=3;
-        for(int i=0;i<arr.length;i++){
-            for (int i1 = i; i1 < arr2.length; i1++) {
-                if(i!=deletedPosition){
-                    arr2[i1]=arr[i];
-                    ;
-                }
+      int[] arr = {1, 2, 3, 4, 5};
+
+        int index = 2; // the index of the element to be deleted
+
+// shift the elements after the deleted element to the left using a nested for loop
+        for (int i = index; i < arr.length - 1; i++) {
+            for (int j = i; j < arr.length - 1; j++) {
+                arr[j] = arr[j + 1];
             }
         }
-        System.out.println("Before deletion :" + Arrays.toString(arr));
-        System.out.println("After deletion :" + Arrays.toString(arr2));
+        System.out.println(Arrays.toString(arr));
+// decrease the size of the array by one
+        int[] newArr = new int[arr.length - 1];
+        for (int i = 0; i < newArr.length; i++) {
+            newArr[i] = arr[i];
+        }
+
+        System.out.println(Arrays.toString(newArr));
 
     }
 }
